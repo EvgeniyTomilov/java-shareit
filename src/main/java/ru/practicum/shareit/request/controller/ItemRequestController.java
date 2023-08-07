c22ac10045cff10c1efcaefeb7aace6404f9a520
TomilovES <blat13@gmail.com>
2023-08-07 19:23:11 +0300

    ТЗ 15  fix bag

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -8,7 +8,6 @@ import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
