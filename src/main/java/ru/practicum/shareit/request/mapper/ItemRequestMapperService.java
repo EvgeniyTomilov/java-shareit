0680ab8cead99d3b390135e93a50882969f8d9f1
TomilovES <blat13@gmail.com>
2023-08-08 22:21:15 +0300

    ТЗ 15  добавил интерфейсы

--- src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapperService.java
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapperService.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public interface ItemRequestMapperService {
     ItemRequest prepareForSaveItemRequest(Long requesterId, ItemRequestDto dto);
+
     boolean requesterValidate(Long requesterId);
+
     ItemRequestDto prepareForReturnDto(ItemRequest itemRequest);
+
     List<ItemRequestDto> prepareForReturnListDto(List<ItemRequest> itemRequests);
 }
