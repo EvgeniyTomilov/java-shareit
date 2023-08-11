248885ed26310bb8d4a86c881fc01b2273d19e3b
TomilovES <blat13@gmail.com>
2023-08-11 20:15:46 +0300

    @DataJpaTest и @jsontest

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.*;
-import ru.practicum.shareit.item.model.Item;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
