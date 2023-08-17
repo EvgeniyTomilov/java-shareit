48a59c4553c7982898f64266355b492cb2f00942
EvgeniyTomilov <blat13@gmail.com>
2023-08-17 21:02:32 +0300

    tz 16 HEADER_USER_ID

--- gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -8,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 import java.util.Map;
 
+import static ru.practicum.shareit.utils.HeaderUserIdConst.HEADER_USER_ID;
+
 public class BaseClient {
     protected final RestTemplate rest;
 
@@ -110,7 +112,7 @@ public class BaseClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         if (userId != null) {
-            headers.set("X-Sharer-User-Id", String.valueOf(userId));
+            headers.set(HEADER_USER_ID, String.valueOf(userId));
         }
         return headers;
     }
